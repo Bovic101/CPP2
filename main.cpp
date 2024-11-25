8eddaecac068e948473ccead35b9e06f6622b377
Bovic101 <Victorbolanle72@gmail.com>
2024-11-25 14:14:29 +0100

    main program implematation

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 12:38:54 by vodebunm          #+#    #+#             */
+/*   Updated: 2024/11/25 14:13:10 by vodebunm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClassFixed.hpp"
+
+int main()
+{
+	Fixed a;//default const
+	Fixed b(a); //copy const
+	Fixed c;
+
+	c = b; //copy assignment op
+	
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	
+	return(0);
+}
